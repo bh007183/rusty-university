8457f6e932cb0be12789ed0a3ffd2b8988adf9da
Ben Hopkins <bjhops17@gmail.com>
2021-11-11 11:17:56 -0800

    added code

---
+++ js-server/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const PORT = process.env.PORT || 8080
+const { ApolloServer } = require('apollo-server-express');
+const sequelize = require("./connection")
+
+
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  ;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+
+sequelize.sync({force: false}).then(()=> {
+    app.listen(PORT, function(){
+        console.log("http://localhost:8080")
+        console.log("http://localhost:8080/graphql")
+    })
+})
+
+}
